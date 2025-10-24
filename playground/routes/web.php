ae1bf99fa009b6af900ce8ffd1256704bd9aeb8a
vienmar <vsogrimen@gmail.com>
2025-10-24 13:58:53 +0800

    Day 1: routes

--- playground/routes/web.php
+++ playground/routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use Illuminate\Routing\RouteUri;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('user',function () {
+    return"<h1>user page</h1>";
+});
+
+Route::prefix('/customer')->name('user.')->group(function () {
+    Route::get('/profile/{id?}',function ($id=null) {
+        return "User profile page for User ID: ".$id;
+    })->name('profile');
+    Route::get('/dashboard',function () {
+        return "user dashboard page";
+    })->name('dashboard');
+    Route::get('/settings',function () {
+        return "user settings page";
+    })->name('settings');
+});
+
+// Route::redirect('/from','/to');
+
+Route::get('/from', function () {
+    return redirect()->route('user.profile');
+});
+
+
