d8fac02bf1001eed5d3590f1c4da6eda774e65dd
Ian Mark Sangcap <78346259+ianmarksangcap@users.noreply.github.com>
2025-10-25 17:49:00 +0800

    Update input-field.blade.php

--- playground/resources/views/components/input-field.blade.php
+++ playground/resources/views/components/input-field.blade.php
@@ -6,7 +6,7 @@
 <div>
     <label for="{{ $name }}" class="block text-sm/6 font-medium text-gray-100 ">{{ $label }}</label>
     <div class="mt-2">
-        <input id="{{ $name }}" type="{{ $type }}" name="{{ $name }}" @isset($required) required @endisset autocomplete="{{ $name }}" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+        <input id="{{ $name }}" type="{{ $type }}" name="{{ $name }}" value="{{ old($name) }}" @isset($required) required @endisset autocomplete="{{ $name }}" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
     </div>
     @error($name)
     <p class="text-red-500"> {{ $message }}</p>
